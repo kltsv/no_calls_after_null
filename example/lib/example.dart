8f7592ff3ef98b482c3229098b8478e5dd282d18
Sergey Koltsov <srg.koltsov@yandex.ru>
2022-10-23 09:03:31 +0500

    Add an empty example package

---
+++ example/lib/example.dart
@@ -0,0 +1,3 @@
+library no_calls_after_null_example;
+
+export 'src/example.dart';
--- example/no_calls_after_null_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:no_calls_after_null/no_calls_after_null.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
